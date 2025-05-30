8ccc67b2852fd9e9d4378031de54111cb4b41f54
Mario6532 <mario.danioni@uc.cl>
2025-05-30 12:18:11 -0400

    arreglo restricciones

--- old_main.py
+++ old_main.py
@@ -77,11 +77,11 @@ def construir_modelo(data):
     
     ### echo = #
     model.update()
-    model.addConstrs((data["e_v"][v] * quicksum(data["N_r"][r] * w[t,v,r]) >= quicksum(x[t,v,p] * data["d_pt"][p,t]) - data["L_tv"][t,v] for v in data["V"] for t in data["T"]), name = "1" )# 
+    model.addConstr((data["Epsilon_v"][v] * quicksum(data["Nabla_r"][r] * w[t,v,r] for r in data["R"]) >= quicksum(x[t,v,p] * data["d_pt"][p,t] for p in data["P"]) - data["L_t"][t,v] for v in data["V"] for t in data["T"]), name = "1" )# 
 
-    model.addConstrs((w[t,v,r] <= alpha[t,v] for t in data["T"] for v in data["V"] for r in data["R"]), name = "2")# 
+    model.addConstr((w[t,v,r] <= alpha[t,v] for t in data["T"] for v in data["V"] for r in data["R"]), name = "2")# 
 
-    model.addConstrs((w[t,v,r] <= data["M"] * e[t,v,r] for t in data["T"] for v in data["V"] for r in data["R"]), name = "3")#
+    model.addConstr((w[t,v,r] <= data["M"] * e[t,v,r] for t in data["T"] for v in data["V"] for r in data["R"]), name = "3")#
 
     model.addConstr((w[t,v,r] >= alpha[t,v] - data["M"] * (1-e[t,v,r]) for t in data["T"] for v in data["V"] for r in data["R"]), name = "4")#
 
@@ -91,9 +91,9 @@ def construir_modelo(data):
 
     model.addConstr((x[t-1,v,p] + y[1,v,p] == x[t,v,p] + z[t,v,p] for t in data["T"] for v in data["V"] for p in data["P"]), name = "7")   #
 
-    model.addConstr((x[0,v,p] + y[t,v,p] == x[t,v,p] + z[t,v,p] for t in data["T"] for v in data["V"] for p in data["P"]), name = "8")#
+    model.addConstr((x[0,v,p] + y[1,v,p] == x[1,v,p] + z[1,v,p] for v in data["V"] for p in data["P"]), name = "8")#
 
-    model.addConstr((data["Q_tv"][t,v] >= quicksum(B[t,v,r] * data["K^r_tv"] for r in data["R"]) + quicksum(y[t,v,p] * data["K^y_tv"] + z[t,v,p] * data["K^z_tv"]) for t in data["T"] for v in data["V"]), name = "9") # 
+    model.addConstr((data["Q_t"][t,v] >= quicksum(B[t,v,r] * data["K_r"] * data["A_v"][v] for r in data["R"]) + quicksum(y[t,v,p] * data["K_p"]) for t in data["T"] for v in data["V"]), name = "9") # 
 
     model.addConstr((quicksum(b[t,v,r] for r in data["R"]) <= 1 for t in data["T"] for v in data["V"]), name = "10")##
 
@@ -101,35 +101,35 @@ def construir_modelo(data):
 
     model.addConstr((e[t,v,r] <= b[t,v,r] + 1- quicksum(b[t, v, r_primo] for r_primo in data["R"] if r_primo != r) for t in data["T"] for r in data["R"] if r_primo != r), name = "12")# listo pero revisar si corre
 
-    model.addConstr((e[t,v,r] <= e[t-1,v,r] + quicksum(b[t, v, r_primo] for r_primo in data["R"] if r_primo != r) for t in data["T"] for r in data["R"] if r_primo != r), name = "13")# listo pero revisar si corre
+    model.addConstr((e[t,v,r] <= e[t-1,v,r] + b[t, v, r] for r in data["R"] for t in data["T"] for r in data["R"]), name = "13")# listo pero revisar si corre
 
-    model.addConstr((e[t,v,r] >= e[t-1,v,r] - quicksum(b[t, v, r_primo] for r_primo in data["R"] if r_primo != r) for t in data["T"] for r in data["R"] if r_primo != r), name = "13")# listo pero revisar si corre
+    model.addConstr((b[t, v, r] <= 1- e[t-1,v,r] for r in data["R"] for t in data["T"] for r in data["R"] if r_primo != r), name = "14")# listo pero revisar si corre
 
-    model.addConstr((quicksum(w[i] for i in data["I"]) <= data["N"]), name = "15") 
+    model.addConstr((quicksum(e[0,v,r] == 1 for r in data["R"]) for v in data["V"]), name = "15") 
 
-    model.addConstr((y[t,v,p] <= data["M"] * data["B^y_tp"][t,v] for t in data["T"] for v in data["V"] for p in data["P"]), name = "16") #
+    model.addConstr((y[t,v,p] <= data["M"] * data["B_pt"][t,p] for t in data["T"] for v in data["V"] for p in data["P"]), name = "16") #VOY ACA
 
-    model.addConstr((H[t,v] >= data["D^h_tv"][t,v] for t in data["T"] for v in data["V"]), name = "17")#
+    model.addConstr((H[data["T"][-1],v] >= data["D_tv"][data["T"][-1],v] for v in data["V"]), name = "17")#####DUdable
 
-    model.addConstr((H[0,v] >= data["D^h_tv"][0,v] for v in data["V"]), name = "18")#
+    ####model.addConstr((H[0,v] >= data["D_tv"][0,v] for v in data["V"]), name = "18")#DUDABLE 
 
-    model.addConstr((H[t,v] >= data["X"] * N[t,v] + data["beta"] * s[t,v] - data["gamma"] * PI[t,v] for v in data["V"] for t in data["T"]), name = "19")#
+    model.addConstr((H[t,v] == data["Chi"] * N[t,v] + data["beta"] * s[t,v] - data["gamma"] * PI[t,v] for v in data["V"] for t in data["T"]), name = "18")#
 
-    model.addConstr((quicksum(y[t,v,p] for P in data["P"]) <= PI[t,v] * data["M"] for v in data["V"] for t in data["T"]), name = "20")#
+    model.addConstr((quicksum(y[t,v,p] for P in data["P"]) <= PI[t,v] * data["M"] for v in data["V"] for t in data["T"]), name = "19")#
 
-    model.addConstr((quicksum(y[t,v,p] for P in data["P"]) <= data["b^y_tp"][t,v] * data["M"] for v in data["V"] for t in data["T"]), name = "21")#
+    model.addConstr((quicksum(y[t,v,p] for P in data["P"]) <= data["B_pt"][t,p] * data["M"] for v in data["V"] for t in data["T"] for p in data["P"]), name = "20")#  DUDABLE 
 
-    model.addConstr((quicksum(z[t,v,p] for P in data["P"]) <= PI[t,v] * data["M"] for v in data["V"] for t in data["T"]), name = "22")#
+    model.addConstr((quicksum(z[t,v,p] for P in data["P"]) <= PI[t,v] * data["M"] for v in data["V"] for t in data["T"]), name = "21")#
 
-    model.addConstr((s[t,v] * data["A_v"][v] == quicksum(x[t,v,p] * data["a_p"][p] for p in data["P"]) for t in data["T"] for v in data["V"]), name = "23")#
+    model.addConstr((s[t,v] * data["A_v"][v] == quicksum(x[t,v,p] * data["a_p"][p] for p in data["P"]) for t in data["T"] for v in data["V"]), name = "22")#
 
-    model.addConstr((data["A_v"][v] >= quicksum(x[t,v,p] * data["a_p"][p] for p in data["P"]) for t in data["T"] for v in data["V"]), name = "24")#
+    model.addConstr((data["A_v"][v] >= quicksum(x[t,v,p] * data["a_p"][p] for p in data["P"]) for t in data["T"] for v in data["V"]), name = "23")#
 
-    model.addConstr((quicksum(x[t,v,p] * data["chi_tp"][t,p] for p in data["P"]) >=  data["M"] * q[t,v,tau] for t in data["T"] for v in data["V"] for tau in data["Tau"]), name = "25")#
+    model.addConstr((quicksum(x[t,v,p] * data["ζ_τp"][t,p] for p in data["P"]) <=  data["M"] * q[t,v,tau] for t in data["T"] for v in data["V"] for tau in data["Tau"]), name = "24")#
 
-    model.addConstr((quicksum(x[t,v,p] * data["chi_tp"][t,p] for p in data["P"]) <= q[t,v,tau] for t in data["T"] for v in data["V"] for tau in data["Tau"]), name = "26")#
+    model.addConstr((quicksum(x[t,v,p] * data["ζ_τp"][t,p] for p in data["P"]) >= q[t,v,tau] for t in data["T"] for v in data["V"] for tau in data["Tau"]), name = "25")#
 
-    model.addConstr((N[t,v] == quicksum(q[t,v,tau] for tau in data["Tau"]) for v in data["V"] for t in data["T"]), name = "27") #
+    model.addConstr((N[t,v] == quicksum(q[t,v,tau] for tau in data["Tau"]) for v in data["V"] for t in data["T"]), name = "26") #
 
 
     f_objetivo = quicksum(quicksum(alpha[t,v] for t in data["T"]) for v in data["V"]) ####
